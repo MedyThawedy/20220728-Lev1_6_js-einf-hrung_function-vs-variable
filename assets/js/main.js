dca2b52c936157cd16fd96ea594e014453c40f3e
MD <dhmedy@googlemail.com>
2022-07-29 09:28:45 +0200

    lc

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,13 +5,20 @@ function returnOne() {
 let x = 1;
 
 let y = function returnOne() {
-    console.log(y + 'Was sent')
+    console.log(y + 'Was sent');
+    console.log(y);
+    console.log(typeof (y));
 }
 
 if (x === y) {
     console.log("Wird das gedruckt?");
+    console.log(y);
+    console.log(typeof (y));
 }
 
 if (x != y) {
     console.log("x is not equal to y");
+    console.log(y);
+    console.log(typeof (y));
 }
+
